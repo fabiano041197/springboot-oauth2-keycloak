a84e03e5dac1b9a07aa7d5ccd6b3f441e8b839a5
fabiano041197 <fabiano.gabardo.lemos@gmail.com>
2024-07-05 21:09:52 -0300

    Initial commit

---
+++ src/main/java/com/gabardo/oauth_demoapp/OauthDemoappApplication.java
@@ -0,0 +1,13 @@
+package com.gabardo.oauth_demoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthDemoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthDemoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabardo/oauth_demoapp/OauthDemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabardo.oauth_demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthDemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
